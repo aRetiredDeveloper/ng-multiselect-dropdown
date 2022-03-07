5dd9bbad0b9d1767a732c7c3bd96c437d048dfe7
Joydeep Banerjee <jdbanerjee2012@gmail.com>
2022-03-07 21:22:24 +0530

    Fixed issue when there is no valid option in list, eventClickoutside directive and exported DropDownConfig

--- projects/ng-simple-multiselect-dropdown/src/lib/event.directive.ts
+++ projects/ng-simple-multiselect-dropdown/src/lib/event.directive.ts
@@ -1,14 +1,14 @@
 import {Directive, ElementRef, Output, EventEmitter, HostListener} from '@angular/core';
 
 @Directive({
-    selector: '[eventOutsideClickDirective]'
+    selector: '[eventClickOutside]'
 })
-export class EventOutsideClickDirective {
+export class EventClickOutsideDirective {
     constructor(private _elementRef: ElementRef) {
     }
 
     @Output()
-    public eventOutside = new EventEmitter<MouseEvent>();
+    public eventClickOutside = new EventEmitter<MouseEvent>();
 
     @HostListener('document:click', ['$event', '$event.target'])
     public onClick(event: MouseEvent, targetElement: HTMLElement): void {
@@ -17,7 +17,7 @@ export class EventOutsideClickDirective {
         }
         const clickedInside = this._elementRef.nativeElement.contains(targetElement);
         if (!clickedInside) {
-            this.eventOutside.emit(event);
+            this.eventClickOutside.emit(event);
         }
     }
 }
